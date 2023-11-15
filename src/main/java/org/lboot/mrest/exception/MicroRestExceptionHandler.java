78c35bd1cc7db94cff7616b91452ee12cc113ce6
kindear <kindear@foxmail.com>
2023-11-15 23:04:47 +0800

    自定义exception 拦截打印错误栈信息

--- src/main/java/org/lboot/mrest/exception/MicroRestExceptionHandler.java
+++ src/main/java/org/lboot/mrest/exception/MicroRestExceptionHandler.java
@@ -1,15 +1,18 @@
 package org.lboot.mrest.exception;
 
 import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class MicroRestExceptionHandler {
     @ExceptionHandler(MicroRestException.class)
     public ResponseEntity<Object> handleMicroRequestException(MicroRestException e) {
+        log.error(e.getMessage(),e);
         HttpStatus status = HttpStatus.resolve(e.code);
         if (status != null){
             return new ResponseEntity<>(JSONUtil.parseObj(e.getMessage()), status);
